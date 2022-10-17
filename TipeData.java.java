37911fc735ca18b6f052677a73ec33c54554d1d4
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-17 23:42:15 +0800

    first commit

---
+++ TipeData.java.java
@@ -0,0 +1,20 @@
+
+package Day1;
+
+
+public class TipeData {
+    public static void main(String[] args) {
+        String nama = "Nur halisah" ;
+        String alamat = "Pasangkayu" ;
+        int umur = 19 ;
+        double beratBadan = 43.5 ;
+        double tinggiBadan = 155.5 ;
+        
+        System.out.println("NAMA : "+ nama);
+        System.out.println("ALAMAT : "+ alamat);
+        System.out.println("Usia : "+ umur);
+        System.out.println("BERAT BADAN :" +beratBadan );
+        System.out.println("tinggiBadan :"+tinggiBadan);
+    }
+    
+}
