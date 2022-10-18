9415f6ae4a02dca363c35202f2d9675ee1520565
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-18 19:42:06 +0800

    haridua

---
+++ Day2.java
@@ -0,0 +1,13 @@
+
+package day2;
+
+
+public class Day2 {
+    public static void main(String[] args) {
+        //Dekalrasi Variabel
+        short angka;
+        angka = 13003 ;
+        //INISIALISASI Variabel
+        System.out.println("Nilai dari short = "+angka);
+    }
+}
