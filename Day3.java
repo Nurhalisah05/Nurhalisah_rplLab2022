05ae8f2a4e4db18e2dc2aa28d19df2a680dcf098
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-19 19:43:37 +0800

    hari ketiga

---
+++ Day3.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Day3 {
+    public static void main(String[] args) {
+        String Nama = "Nur halisah";
+        char HurufAwalan ='N'; //Char Hanya dapat Menampung satu Karakter
+        
+        System.out.println("Nama : "+Nama);
+        System.out.println("Inisial Nama : "+HurufAwalan);
+    }
+    
+}
