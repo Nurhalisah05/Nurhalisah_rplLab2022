74165db8d5887b7b77cd0ce7145764232850ddc0
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-20 19:40:38 +0800

    Hari Ke 4

---
+++ Day4.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class Day4 {
+
+    public static void main(String[] args) {
+        //VARIABEL
+        System.out.println("== BIO DATA ==");
+        String Nama = "Nur halisah";
+        String Alamat = "Bambalamotu";
+        int TglLahir = 5;
+        String BulanLahir = "Oktober";
+        int ThnLahir = 2003;
+        double TinggiBadan = 153.5;
+        double BrtBadan = 43.5;
+        char GolDarah = 'O';
+        char Jenis_Kelamin = 'P';
+        boolean Status_Perkawinan = false;
+        
+        System.out.println("NAMA LENGKAP : "+ Nama);
+        System.out.println("ALAMAT : "+ Alamat);
+        System.out.println("TANGGAL LAHIR : "+TglLahir+' '+BulanLahir+' '+ThnLahir);
+        System.out.println("TINGGI BADAN : "+ TinggiBadan);
+        System.out.println("BERAT BADAN : "+ BrtBadan);
+        System.out.println("GOLONGAN DARAH : "+GolDarah);
+        System.out.println("JENIS KELAMIN : "+ Jenis_Kelamin);
+        System.out.println("STATUS PERKAWINAN : "+Status_Perkawinan);
+    }
+}
