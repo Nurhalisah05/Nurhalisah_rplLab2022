5ac532e1a522d0c9e801412a67d61e9b088e9742
Nurhalisah05 <Nurhalisah05>
2022-10-21 18:54:05 +0800

    Aritmatika Dasar

---
+++ Day5.java
@@ -0,0 +1,18 @@
+
+package day2;
+
+public class Day5 {
+    public static void main(String[] args) {
+        //OPERASI ARITMATIKA
+        int a = 22;
+        int b = 13;
+        
+        System.out.println("HASIL (PENJUMLAHAN): "+( a + b));//PENJUMLAHAN
+        System.out.println("HASIL (PENGURANGAN) : "+(a - b));//PENGURANGAN
+        System.out.println("HASIL (PERKALIAN ): "+(a * b));     //PERKALIAN 
+        System.out.println("HASIL (PEMBAGIAN): "+(a / b)); //PEMBAGIAN
+        System.out.println("HASIL (SISA BAGI): "+(a % b));  //SISA BAGI
+        
+    }
+    
+}
