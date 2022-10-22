3e2a4ea2e0b2582510cb7653ce333b1f1b85dacc
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-22 21:44:09 +0800

    Day 6

---
+++ Day6.java
@@ -0,0 +1,14 @@
+
+package day2;
+
+public class Day6 {
+    public static void main(String[] args) {
+        String [] nama = {"Lisa","Marwa","Dipa","wiwi","Kunde(Berliana saphira)"};
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+        System.out.println(nama[3]);
+        System.out.println(nama[4]);
+        
+    }
+}
