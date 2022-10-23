12546d606084fdfa64682a7cac43df33287d9b6d
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-23 20:42:21 +0800

    Day7

---
+++ Day7.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Selamat Siang");
+        } else {
+            System.out.println("Selamat Malam");
+        }
+    }
+}
