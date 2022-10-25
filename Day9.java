abf0c685ded0097037973ec52d304d5f8d5c9eab
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-25 21:08:48 +0800

    Day9

---
+++ Day9.java
@@ -0,0 +1,12 @@
+
+package day2;
+
+public class Day9 {
+    public static void main(String[] args) {
+        for (int hitungan = 0; hitungan < 99; hitungan++) {
+            System.out.println("Nur halisah");
+            
+        }
+    }
+    
+}
