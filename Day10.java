9ec91b7f04d6ffef0f2b32651dc9f2c8e016e499
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-26 20:49:56 +0800

    Day10

---
+++ Day10.java
@@ -0,0 +1,27 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class Day10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+ 
+        String nama, nim, Kelas,Fakultas,Prodi , Universitas = "Masukkan ";
+        
+        System.out.println("MASUKKAN NAMA : ");
+        nama = input.nextLine();
+        System.out.println("MASUKKAN NIM :");
+        nim = input.nextLine();
+        System.out.println("MASUKKAN KELAS :");
+        Kelas = input.nextLine();
+        System.out.println("MASUKKAN FAKULTAS : ");
+        Fakultas = input.nextLine();
+        System.out.println("MASUKKAN PRODI : ");
+        Prodi = input.nextLine();
+        System.out.println("MASUKKAN NAMA KAMPUS");
+        Universitas = input.nextLine();
+                
+    }
+    
+}
