b07d211de49e45b712027bc56a3060520571d522
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-28 23:26:42 +0800

    Day12

---
+++ Day12.java
@@ -0,0 +1,17 @@
+
+package day2;
+
+
+public class Day12 {
+    public static void main(String[] args) {
+        int T = 30;
+        if (T < 0){
+            System.out.println("Wujud air adalah beku "+ T);
+        }else if ((0 <= T) &&(T <= 100)){
+            System.out.println("Wujud air adalah cair" + T);
+        }else if (T > 100){
+            System.out.println("Wujud air berupa gas atau uap"+ T);
+        }
+    }
+    
+}
