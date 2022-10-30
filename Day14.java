a444a9345ac7adec642a4b0c078f7e38fc523919
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-10-30 22:20:01 +0800

    Day14

---
+++ Day14.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+        for (int hitungan = 0; hitungan <= 1000; hitungan++) {
+            System.out.println("BELAJAR NGODING");
+        }
+    }
+
+}
