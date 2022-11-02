d2e3276753feed27de766b064d7aa49cda219c53
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-11-02 21:12:23 +0800

    Day17

---
+++ Day17.java
@@ -0,0 +1,17 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class Day17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Nama = "MASUKKAN NAMA : ";
+        System.out.print(Nama);
+        String str = sc.nextLine();
+        System.out.println("NAMA : "+str);
+        
+        
+    }
+    
+}
