3ccc527199eaeeb36bd18d79a0459be0e4bdb93e
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-11-06 21:53:56 +0800

    Day21

---
+++ Day21.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Day21 {
+
+    static void myMethod() {
+        System.out.println("CONTOH PENGGUNAAN METHOD");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+
+    }
+}
