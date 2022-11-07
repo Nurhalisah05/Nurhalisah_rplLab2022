0e05047c2ea2d66cb475d1f1630d1ca8fb49cc45
Nurhalisah05 <nurhalizah05102003@gmail.com>
2022-11-07 23:17:25 +0800

    Day22

---
+++ Day22.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+}
