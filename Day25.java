c8fd7136f104bfeff0938f46d55934cf9156b042
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-10 20:19:26 +0800

    inputan

---
+++ Day25.java
@@ -0,0 +1,16 @@
+
+package day2;
+
+import java.util.Scanner;
+
+public class Day25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        double nilaiPretest = sc.nextDouble();
+        if (nilaiPretest > 50){
+        }else{
+        System.out.println("TIDAK LULUS ");
+    }
+    }
+    
+}
