2a57c291c336bbf6f4ac30ca51723b96c8107968
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-13 21:53:13 +0800

    menhasilkan nomor acak

---
+++ Day28.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Day28 {
+
+    public static void main(String args[]) {
+        System.out.println("NOMOR ACAK ");
+        System.out.println("1st Random Number: " + Math.random());
+        System.out.println("2nd Random Number: " + Math.random());
+        System.out.println("3rd Random Number: " + Math.random());
+
+    }
+}
