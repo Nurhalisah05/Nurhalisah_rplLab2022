03d4ceb83b108f483baa107ba974f1c63c447f57
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-17 20:12:10 +0800

    Menampilkan alfabet

---
+++ Day32.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Day32 {
+    public static void main(String[] args) {
+
+    char c;
+
+    for(c = 'A'; c <= 'Z'; ++c)
+      System.out.print(c + " ");
+    }
+}
