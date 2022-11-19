955fc62ef7dd5df20bbc627d4f2c30d66eed5df7
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-19 22:07:50 +0800

    int to string

---
+++ Day34.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Day34 {
+
+    public static void main(String[] args) {
+        int i = 200;
+        String s = String.valueOf(i);
+        System.out.println(i + 100);//300 karena + adalah operator plus biner
+        System.out.println(s + 100);//200100 karena + adalah operator penggabungan string
+    }
+}
