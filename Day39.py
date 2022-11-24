69d3796e3445f2ce0737e22c82f64cdffd35365d
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-24 09:42:34 +0800

    #contoh kasus program harga setelah diskon

---
+++ Day39.py
@@ -0,0 +1,11 @@
+angka = int (input ("masukkan nilai : "))
+if angka <60 :
+    print ("MENDAPATKAN NILAI D")
+elif angka <70 :
+    print ("MENDAPATKAN NILAI C")
+elif angka <80 :
+    print ("MAKA NILA B")
+elif angka > 90  :
+    print ("MAKA NILAI A")
+else :
+    print ("E")
