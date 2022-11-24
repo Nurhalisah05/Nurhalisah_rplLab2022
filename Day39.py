68f01092fe25e3e96cc6808f77ba716b6f5db00f
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-24 18:59:47 +0800

    contoh kasus program harga setelah diskon

---
+++ Day39.py
@@ -0,0 +1,14 @@
+#contoh kasus program harga setelah diskon
+total = 5000
+setelah_diskon = total
+
+if total < 100000 :
+    diskon = total * (5 /100)
+else:
+    diskon = total * (10/1000)
+
+setelah_diskon = total - diskon
+print ("diskonnya yaitu : ", diskon)
+print ("harga setelah di diskon", setelah_diskon)
+
+
