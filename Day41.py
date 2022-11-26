e72dc5b1936fdebbec80284e59c1aec8b6d81ade
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-26 19:34:06 +0800

    operator aritmatika menggunakan inputan

---
+++ Day41.py
@@ -0,0 +1,12 @@
+x = int (input("Masukkan Angka : "))
+y = int (input ("Masukkan Angka : "))
+
+print ("Hasil Penjumlahan : ", x + y )
+print ("Hasil Pengurangan : ", x - y )
+print ("Hasil Perkalian : ", x * y )
+print ("Hasil Pembagian : ", x / y )
+print ("0PERATOR ARITMATIKA MENGGUANKAN INPUTAN")
+
+
+
+
