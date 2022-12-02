991fb7bcc852bf0a9ad2bb1494c735fe373f79f4
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-02 22:15:01 +0800

    Operator Perbandingan

---
+++ Day48.py
@@ -0,0 +1,14 @@
+x = 7 
+y = 10
+ 
+print ("OPERATOR PERBANDINGAN\n ")
+print('x =',x)
+print('y =',y)
+print('\n')
+ 
+print('x == y hasilnya', x==y)
+print('x != y hasilnya', x!=y)
+print('x > y  hasilnya', x>y)
+print('x < y  hasilnya', x<y)
+print('x >= y hasilnya', x>=y)
+print('x <= y hasilnya', x<=y)
