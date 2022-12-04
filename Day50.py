ba9e0a08efc67fe106724c9d3eaf50fdff0d90ad
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-04 18:33:15 +0800

    Casting Tipe Data

---
+++ Day50.py
@@ -0,0 +1,61 @@
+#Casting
+#Casting Adalah mengubah tipedata ke tipe data lainny
+# tipe data , Int, float , string , boolean
+print("====INTEGER====")
+
+nomor = 40 # INT : Merubah tipe data int ke tipe data lainnya 
+print ("nomor", nomor, ",type", type(nomor))
+
+data_float = float(nomor)
+print ("nomor", data_float, ",type", type(data_float))
+
+data_String = str(nomor)
+print ("nomor", data_String, ",type", type(data_String))
+
+data_boolean = bool(nomor)
+print ("nomor", data_boolean, ",type", type(data_boolean))
+
+print("====FLOAT ====")
+#Float : Merubah Tipe data float ke tipe data lainnya
+angka = 2.0
+print ("angka", angka, ",type", type(angka))
+
+ke_int = int(angka)
+print ("ke_int", ke_int, ",type", type(ke_int))
+
+ke_String = str(angka)
+print ("ke_String", ke_String, ",type", type(ke_String))
+
+ke_boolean = bool(angka)
+print ("ke_boolean", ke_boolean, ",type", type(ke_boolean))
+
+print("==boolean===========")
+angk = False
+print ("angka", angk, ",type", type(angk))
+
+ke_intt = int(angk)
+print ("ke_int", ke_intt, ",type", type(ke_intt))
+
+ke_String = str(angk)
+print ("ke_String", ke_String, ",type", type(ke_String))
+
+ke_float = float(angk)
+print ("ke_float", ke_float, ",type", type(ke_float))
+
+print("====SRING====")
+nomorr = "20"
+print ("nomorr", nomorr, ",type", type(nomorr))
+
+ke_integer = int (nomorr)
+print("ke_integer", ke_integer, ",type", type(ke_integer))
+
+ke_floaat = float(nomorr)
+print("ke_float", ke_floaat, ",type", type(ke_floaat))
+
+ke_booleaan = bool(nomorr)
+print("ke_booleaan", ke_booleaan, ",type", type(ke_booleaan))
+
+
+
+
+
