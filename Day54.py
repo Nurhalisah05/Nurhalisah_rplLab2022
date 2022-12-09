c9fb7de09c6fe95d23d6c67a799798f812e927be
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-09 22:12:05 +0800

    Penggunaan if else

---
+++ Day54.py
@@ -0,0 +1,20 @@
+today = input ("Masukkan Hari : ")
+
+if(today == "Senin"):
+    print("Saya akan kuliah")
+elif(today == "Selasa"):
+    print("Saya akan kuliah")
+elif(today == "Rabu"):
+    print("Saya akan kuliah")
+elif(today == "Kamis"):
+    print("Saya akan kuliah")
+elif(today == "Jumat"):
+    print("Saya akan kuliah")
+elif(today == "Sabtu"):
+    print("Kayaknya Menggalau dulu deh")
+elif(today == "Minggu"):
+    print("Saya akan libur")
+elif (today == "Malam"):
+    print("KERJA TUGAS ATAU BELAJAR")
+else:
+    print("MENGGABUT WALAUPUN BANYAK TUGAS")
