9a99df81f8bc8b64095493d65a1b764a08e9f651
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-10 22:52:34 +0800

    Penggunaan If Else (Jadwal cuci piring sektor BTP)

---
+++ Day55.py
@@ -0,0 +1,18 @@
+jadwal = input ("Masukkan Hari : ")
+print ("JADWAL CUCI PIRING SEKTOR BTP")
+if(jadwal == "Senin"):
+    print("WIWI LESTIANI KEJORA")
+elif(jadwal == "Selasa"):
+    print("DIPA DIPO")
+elif(jadwal == "Rabu"):
+    print("MARWA MAWWA")
+elif(jadwal == "Kamis"):
+    print("BERLIAN KUNDE")
+elif(jadwal == "Jumat"):
+    print("LISA BETI")
+elif(jadwal == "Sabtu"):
+    print("MARWA DIPA KUNDE")
+elif(jadwal == "Minggu"):
+    print("BETI AND KEJORA")
+else:
+    print("TAPI ITU HANYALAH SEKEDAR FORMALITAS ")
