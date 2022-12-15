094a9dab8080b73a3a7a9c211aa57f19ec9dbecc
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-15 23:33:33 +0800

    menukar nila variabel

--- Day60.py
+++ Day60.py
@@ -1,9 +1,5 @@
-u = [3, 2, -1]
-w = [2, 6, 7]
-v = [0, 2, -3]
-uw = [20, -23, 14]
+gelas_a = 'kopi'
+gelas_b = 'teh'
 
-uKaliW = ((u[1] * w[2] - u[2] * w[1]),(u[2] * w[0] - u[0] * w[2]),(u[0] * w[1] - u[1] * w[0]))
-print("hasil perkalian silang dari u dan w : ",uKaliW)
-hasil = ((u[1]*uKaliW[2] - u[2]*uKaliW[1]), (u[2]*uKaliW[0] - u[0]*uKaliW[2]),(u[0]*uKaliW[1] - u[1]*uKaliW[0]))
-print ("hasil perkalian silang dari u dan hasil perkalian dari u dan v : ", hasil)
+print('[nilai awal]')
+print(f'gelas_a = {gelas_a}, gelas_b = {gelas_b}\n')
