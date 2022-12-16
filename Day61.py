98ffc0f794eaffeb5b12341831b34d638f6a6cac
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-16 22:12:59 +0800

    operas logika or dan not

---
+++ Day61.py
@@ -0,0 +1,18 @@
+#operasi Logika
+#not , or
+
+
+#INI ADALAH NOT
+print("==NOT==")
+a = False
+c = not a
+print("Data a =", a)
+print("================NOT")
+print ("data c =", c)
+
+#INI ADALAH OR
+print("====OR====")
+a = False
+b = False
+c = a or b
+print(a,"OR",b,"=",c)
