84dd16cc981287a8a95851d4a23624e86aa49e46
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-25 17:16:05 +0800

    Menghitung panjang string

---
+++ Day69.py
@@ -0,0 +1,11 @@
+#Menyambung String
+nama_pertama = "Nur"
+nama_tengah = "Halisah"
+nama_akhir = "Lisa"
+
+nama_Lengkap = nama_pertama +nama_tengah + nama_akhir 
+print(nama_Lengkap)
+
+#MENGHITUNG PANJANG STRING
+panjang = len (nama_Lengkap)
+print (panjang)
