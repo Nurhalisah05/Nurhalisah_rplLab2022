61e148e2464836ac4fbb479a16fec32b9e4db87e
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-12-27 17:08:05 +0800

    menghitung semen

--- Day72.py
+++ Day72.py
@@ -1,9 +1,12 @@
 #Menghitung Harga Total Penjualan
 HrgPerSakSemen = 60000 
-JumalahSemenYgDiBeli = int(input("Masukkan Jumlah Semen yang Dibei"))
-if JumalahSemenYgDiBeli > 100 :
-    print("Mendapatkan Potongan Sebesar 2%", JumalahSemenYgDiBeli - 2/100)
-elif JumalahSemenYgDiBeli > 200 :
-    print ("Mendapatkan potongan sebesar 5%", JumalahSemenYgDiBeli - 5/100)
+JumalahSemenYgDiBeli = int(input("Masukkan Jumlah Semen yang Dibei : "))
+HrgSemenYgDibeli = HrgPerSakSemen * JumalahSemenYgDiBeli
+Diskon1 = 2/100 #Sama Dengan 2%
+Diskon2 = 5/100 #Sama Dengan 5%
+if JumalahSemenYgDiBeli >=200  :
+    print("Mendapatkan Potongan Sebesar 5%, Total yang Dibayar :Rp.",HrgSemenYgDibeli - Diskon2)
+elif JumalahSemenYgDiBeli >= 100 :
+    print ("Mendapatkan Potongan Sebesar 2%, Total yang Dibayar :Rp.",HrgSemenYgDibeli - Diskon1)
 else:
-    print("Tidak Mendapatjan Diskon")
+    print("Tidak Mendapatkan Diskon")
